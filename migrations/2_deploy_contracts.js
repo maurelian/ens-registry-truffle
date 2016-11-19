98f74a4a6898de1c18594aabb21bc70c605efd8f
maurelian <leighvert@protonmail.ch>
2016-11-19 15:00:30 +0100

    ignore npm modules

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,7 @@
+var ens = require('../node_modules/ethereum-ens');
+var ethHash = ens.prototype.namehash("eth")
+
 module.exports = function(deployer) {
-  // deploy the ENS with the owner as my TestRPC coinbase address
-  deployer.deploy(ENS, "0x1a1c961856b5d002428eb97241bf0c6731039ca9");   
-  deployer.deploy(Registrar, 
-            ENS.address, 
-            "0xd1b0e2eec983ad6a7fb21f6fc706af8717b12b8814d2596016750ea73e00b57f"
-    );
+  deployer.deploy(ENS, web3.eth.accounts[0]);   
+  deployer.deploy(Registrar, ENS.address, ethHash);
 };
